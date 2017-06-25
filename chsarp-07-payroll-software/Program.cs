521b75a0e98f5785fabcc0ea381351df26a0ec5b
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-24 21:50:18 -0500

    Admin class added

--- chsarp-07-payroll-software/Program.cs
+++ chsarp-07-payroll-software/Program.cs
@@ -81,6 +81,31 @@ namespace chsarp07payrollsoftware
     }//End Manager Child Class
 
 
+    //Admin : Staff Class
+    class Admin : Staff
+    {
+        private const float overtimeRate = 15.5;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime { get; private set; }
+        public Admin(string name) : base(name, adminHourlyRate){}
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if (HoursWorked > 160){
+                Overtime = overtimeRate * (HoursWorked - 160);
+                TotalPay = BasicPay + Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Admin:\n -Overtime={0}]", Overtime);
+        }
+    }//Admin : Staff Class end
+
+
     class MainClass
     {
         public static void Main(string[] args)
